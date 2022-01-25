358f71ef6b0f9aa80063e17afc674593c878768a
Ankit Gupta <ankit.gupta_cs.aiml20@gla.ac.in>
2022-01-25 11:53:40 +0530

    insert at first and last in singlyLinkedList

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -15,12 +15,50 @@ public class SinglyLinkedList {
     public void display()
     {
         ListNode current = head;
+        int count=0;
         while(current != null)
         {
             System.out.print(current.data +"-->");
+            count++;
             current = current.next;
         }
-        System.out.print("null");
+        System.out.println("null");
+    }
+
+    public void insertFirst(int value){
+        ListNode newNode = new ListNode(value);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void insertLast(int value)
+    {
+        ListNode newNode = new ListNode(value);
+        if(head == null)
+        {
+            head = newNode;
+            return;
+        }
+        ListNode current = head;
+        while(null != current.next)
+        {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    public int length()
+    {
+        if(head == null){
+            return 0;
+        }
+        int count=0;
+        ListNode current = head;
+        while(current != null)
+        {
+            count++;
+            current = current.next;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
@@ -35,6 +73,12 @@ public class SinglyLinkedList {
         third.next = fourth;
 
         sll.display();
+        System.out.println("Length is - "+sll.length());
 
+        sll.insertFirst(115);
+        sll.display();
+
+        sll.insertLast(88);
+        sll.display();
     }
 }
